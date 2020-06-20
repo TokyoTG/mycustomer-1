0802eff5d68cf9f0c464917048362927e9d49bc5
Rod-Rikk <daviddowuona9.dd@gmail.com>
2020-06-20 18:47:21 +0000

    Make container fluid for transaction views

--- resources/views/backend/transactions/index.blade.php
+++ resources/views/backend/transactions/index.blade.php
@@ -6,7 +6,7 @@
 
 
 <div class="account-pages my-5">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row-justify-content-center">
             <div class="h2"><i data-feather="file-text" class="icon-dual"></i> Transaction Center</div>
 
